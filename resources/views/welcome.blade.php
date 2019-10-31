055458bfae8a1b678f1cee52da1c5d3def258b1c
rbuzzibarrios <rbuzzibarrios@gmail.com>
2019-10-31 10:42:40 -0400

    modify title, show hello-title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,9 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel RBB_MY_TITLE</title>
+        <title>
+            Laravel RBB-HELLO_TITLE
+        </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
