12103d27e06d7dad843e984c5d7ad6dc360dca80
rbuzzibarrios <rbuzzibarrios@gmail.com>
2019-10-31 10:37:48 -0400

    test modify my title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>
-            Laravel RBB-HELLO_TITLE
+            Laravel RBB_MY_TITLE
         </title>
 
         <!-- Fonts -->
