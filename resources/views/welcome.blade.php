f5891a639f4a0244b11ec30ba064a67f71a43690
rbuzzibarrios <rbuzzibarrios@gmail.com>
2019-10-31 10:37:48 -0400

    test modify my title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel RBB</title>
+        <title>Laravel RBB_MY_TITLE</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
