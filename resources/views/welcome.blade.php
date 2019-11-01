f7ea1abdfc47f0526988cc6f571ea6b07d9bc062
rbuzzibarrios <rbuzzibarrios@gmail.com>
2019-11-01 17:13:04 -0400

    modify blog link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,7 +93,7 @@
                 Laracasts RBB Title
             </a>
             <a href="https://laravel-news.com">News</a>
-            <a href="https://blog.laravel.com">Blog</a>
+            <a href="https://blog.laravel.com">Blog RBB</a>
             <a href="https://nova.laravel.com">Nova</a>
             <a href="https://forge.laravel.com">Forge</a>
             <a href="https://github.com/laravel/laravel">GitHub</a>
